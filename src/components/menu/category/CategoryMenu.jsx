81153bf00d930f717571a4572cc307fc4151bae0
Rana Ahtsham Ul Haq <rana1ahtsham@gmail.com>
2024-06-13 19:14:17 -0400

    menu details

--- src/components/menu/category/CategoryMenu.jsx
+++ src/components/menu/category/CategoryMenu.jsx
@@ -3,19 +3,27 @@ import React from 'react';
 import './CategoryMenu.css';
 import ProductMenu from "../product/ProductMenu";
 import {languageTranslate} from "../../../util/utils";
+import MenuDetails from "../product/details/MenuDetails";
 
 const CategoryMenu = ({title, categoryData}) => {
     return (
         <div className="category-menu-container" id={title.replace(/\s+/g, '-').toLowerCase()}>
             <h2 className="category-menu-heading">{languageTranslate(title)}</h2>
             {categoryData?.description &&
-                <p style={{fontWeight: "bold"}}>{languageTranslate(categoryData?.description)}</p>}
+                (
+                    <p style={{fontWeight: "bold"}}>{languageTranslate(categoryData?.description)}</p>
+                )
+            }
 
-            <div className="category-menu-grid">
-                {categoryData?.products.map((product, index) => (
-                    <ProductMenu key={index} productData={product}/>
-                ))}
-            </div>
+            {categoryData?.products &&
+                (
+                    <div className="category-menu-grid">
+                        {categoryData?.products.map((product, index) => (
+                            <ProductMenu key={index} productData={product}/>
+                        ))}
+                    </div>
+                )
+            }
         </div>
     );
 }
