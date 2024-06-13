3fe57c33c574a0b08cf9275acef194d610821f95
Rana Ahtsham Ul Haq <rana1ahtsham@gmail.com>
2024-06-13 18:48:11 -0400

    main dishes combo

--- src/components/menu/category/CategoryMenu.jsx
+++ src/components/menu/category/CategoryMenu.jsx
@@ -8,6 +8,9 @@ const CategoryMenu = ({title, categoryData}) => {
     return (
         <div className="category-menu-container" id={title.replace(/\s+/g, '-').toLowerCase()}>
             <h2 className="category-menu-heading">{languageTranslate(title)}</h2>
+            {categoryData?.description &&
+                <p style={{fontWeight: "bold"}}>{languageTranslate(categoryData?.description)}</p>}
+
             <div className="category-menu-grid">
                 {categoryData?.products.map((product, index) => (
                     <ProductMenu key={index} productData={product}/>
