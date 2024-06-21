dbe8aa50cfd1ea7340a0bd836cd0039124c6c958
Rana Ahtsham Ul Haq <rana1ahtsham@gmail.com>
2024-06-21 18:49:45 -0400

    Seekh Kebab (Chicken)

--- src/components/menu/product/ProductMenu.jsx
+++ src/components/menu/product/ProductMenu.jsx
@@ -7,22 +7,25 @@ const ProductMenu = ({productData}) => {
     return (
         <div className="product-menu-container">
             {productData?.is_new && <div className="product-new-banner">New</div>}
+
             {productData?.image_url && (<img src={createImageUrl(productData.image_url)} alt={productData.title}/>)}
 
-            <div className={"product-details-wrapper"}>
-                <p style={{flex: "1"}}>{languageTranslate(productData.title)}</p>
-                {productData?.price && <p style={{color: "#909090"}}>${productData?.price}</p>}
-            </div>
+            <div className={"product-details-container"}>
+                <div className={"product-details-wrapper"}>
+                    <p style={{flex: "1"}}>{languageTranslate(productData.title)}</p>
+                    {productData?.price && <p style={{color: "#909090"}}>${productData?.price}</p>}
+                </div>
 
-            <div className={"product-contents"}>
-                {productData?.description && <p>{languageTranslate(productData?.description)}</p>}
-                {productData?.contents && (
-                    <ul>
-                        {productData.contents.map((item, index) => (
-                            <li key={index}>{languageTranslate(item)}</li>
-                        ))}
-                    </ul>
-                )}
+                <div className={"product-contents"}>
+                    {productData?.description && <p>{languageTranslate(productData?.description)}</p>}
+                    {productData?.contents && (
+                        <ul>
+                            {productData.contents.map((item, index) => (
+                                <li key={index}>{languageTranslate(item)}</li>
+                            ))}
+                        </ul>
+                    )}
+                </div>
             </div>
         </div>
     );
